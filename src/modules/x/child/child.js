8a53f87d166936e17bf41bd5f0868613d6d2dfbd
Dale Bustad <dale@divmain.com>
2023-06-15 13:55:08 -0700

    chore: add console.log to demonstrate the order of events

--- src/modules/x/child/child.js
+++ src/modules/x/child/child.js
@@ -1,17 +1,20 @@
 import { LightningElement, api } from 'lwc';
 
+const env = typeof window === 'undefined' ? 'server' : 'client';
+
 export default class App extends LightningElement {
   _bar = '';
   @api get foo() {
     return ''
   }
   set foo(val) {
+    console.log(`${env}: <x-child>'s "foo" setter is invoked`);
     this._bar = val;
     this.updateClasses();
   }
 
   updateClasses() {
-    debugger;
+    console.log(`${env}: setting class "${this._bar}" on <x-child>'s host element`);
     this.classList?.add(this._bar);
   }
 }
